26319c44342342ccc2ef465926b3e1b6a4459da8
Mohamed Hak <sprado.hak@gmail.com>
2020-01-24 17:06:42 +0100

    updated some stuff

--- Source Control/ViewController.swift
+++ Source Control/ViewController.swift
@@ -17,7 +17,7 @@ class ViewController: NSViewController {
     }
 
     /**
-     Adds two numbers together and returns result.
+     Adds two numbers together and returns result. Modified bucket
      - parameter num1: The first number.
      - parameter num2: the second number.
      - returns: The sum of num1 and num2
